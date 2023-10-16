33ca22ee16645f916bb7de065b91a4e7e08006ab
ĴelianCode <vgtmldnd@gmail.com>
2023-10-15 22:54:09 -0400

    controllers complete

--- src/main/java/jelian/code/springdata/dto/LoginDto.java
+++ src/main/java/jelian/code/springdata/dto/LoginDto.java
@@ -2,10 +2,13 @@ package jelian.code.springdata.dto;
 
 import lombok.Getter;
 
+import lombok.Getter;
+
 @Getter
 public class LoginDto {
 
     private String username;
     private String password;
+    private Integer retryTimes;
 
 }
--- src/main/java/jelian/code/springdata/service/LoginService.java
+++ src/main/java/jelian/code/springdata/service/LoginService.java
@@ -9,31 +9,46 @@ import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
+import jelian.code.springdata.domain.User;
+import jelian.code.springdata.dto.LoginDto;
+import jelian.code.springdata.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class LoginService {
-    private final UserRepository userRepository;
 
-    public Boolean validateLogin(LoginDto loginDto) {
-        return validateUserNameAndPassword(loginDto.getUsername(), loginDto.getPassword());
-    }
+  private final UserRepository userRepository;
+
+  public Boolean validateLogin(LoginDto loginDto) {
+    return validateUserNameAndPassword(loginDto);
+  }
 
 
-    private Boolean validateUserNameAndPassword(String username, String password) {
-        Boolean response = Boolean.FALSE;
-        if (Objects.nonNull(username) && Objects.nonNull(password)) {
-            try {
-                /*insert checkIn storage procedure here*/
-                User user = userRepository.findByUsernameAndUserPasswordAndUserEnable(username, password, true);
-                log.info("success!!!");
-
-                response = Objects.nonNull(user.getIdUser());
-            } catch (Exception e) {
-                log.info("fail!!!");
-            }
+  private Boolean validateUserNameAndPassword(LoginDto loginDto) {
+    Boolean response = Boolean.FALSE;
+    if (Objects.nonNull(loginDto.getUsername()) && Objects.nonNull(loginDto.getPassword())) {
+      try {
+        User user = userRepository.findByUsernameAndUserPasswordAndUserEnable(
+            loginDto.getUsername(), loginDto.getPassword(), true);
+        if (loginDto.getRetryTimes() > 3) {
+          user.setUserEnable(Boolean.FALSE);
+          userRepository.save(user);
+          response = Boolean.FALSE;
+        } else {
+          response = Objects.nonNull(user.getIdUser());
         }
-
-        return response;
+      } catch (Exception e) {
+        log.info("fail!!!");
+      }
     }
+
+    return response;
+  }
+
 }
